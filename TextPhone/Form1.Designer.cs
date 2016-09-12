b225619d750b27ed27fa3af35abd52dcf404961d
SimonLindberg98 <simonlindberg98@gmail.com>
2016-09-12 18:47:08 +0200

    Working on a phone type program
    Features primitive T9, and the old basic way of inputting text on phones

---
+++ TextPhone/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace TextPhone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Backspace = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.wordListBox = new System.Windows.Forms.ListBox();
+            this.t9_checkBox = new System.Windows.Forms.CheckBox();
+            this.txt_radButton = new System.Windows.Forms.RadioButton();
+            this.num_radButton = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(174, 36);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(12, 54);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(82, 23);
+            this.btn_Delete.TabIndex = 1;
+            this.btn_Delete.Text = "|<---";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Backspace
+            // 
+            this.btn_Backspace.Location = new System.Drawing.Point(104, 54);
+            this.btn_Backspace.Name = "btn_Backspace";
+            this.btn_Backspace.Size = new System.Drawing.Size(82, 23);
+            this.btn_Backspace.TabIndex = 2;
+            this.btn_Backspace.Text = "<---";
+            this.btn_Backspace.UseVisualStyleBackColor = true;
+            this.btn_Backspace.Click += new System.EventHandler(this.btn_Backspace_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(13, 85);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(53, 23);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(73, 85);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(53, 23);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "2 abc";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(133, 85);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(53, 23);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "3 def";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(13, 114);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(53, 23);
+            this.btn4.TabIndex = 8;
+            this.btn4.Text = "4 ghi";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(73, 114);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(53, 23);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5 jkl";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(133, 114);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(53, 23);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6 mno";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(13, 143);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(53, 23);
+            this.btn7.TabIndex = 11;
+            this.btn7.Text = "7 pqrs";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(73, 143);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(53, 23);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "8 tuv";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(133, 143);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(53, 23);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9 wxyz";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(74, 172);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(53, 23);
+            this.btn0.TabIndex = 12;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.wordListBox);
+            this.groupBox1.Controls.Add(this.t9_checkBox);
+            this.groupBox1.Controls.Add(this.txt_radButton);
+            this.groupBox1.Controls.Add(this.num_radButton);
+            this.groupBox1.Location = new System.Drawing.Point(211, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(205, 183);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inmatningsalternativ";
+            // 
+            // wordListBox
+            // 
+            this.wordListBox.FormattingEnabled = true;
+            this.wordListBox.Location = new System.Drawing.Point(108, 68);
+            this.wordListBox.Name = "wordListBox";
+            this.wordListBox.Size = new System.Drawing.Size(80, 95);
+            this.wordListBox.TabIndex = 3;
+            // 
+            // t9_checkBox
+            // 
+            this.t9_checkBox.AutoSize = true;
+            this.t9_checkBox.Location = new System.Drawing.Point(108, 44);
+            this.t9_checkBox.Name = "t9_checkBox";
+            this.t9_checkBox.Size = new System.Drawing.Size(83, 17);
+            this.t9_checkBox.TabIndex = 2;
+            this.t9_checkBox.Text = "Ordlista (T9)";
+            this.t9_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // txt_radButton
+            // 
+            this.txt_radButton.AutoSize = true;
+            this.txt_radButton.Location = new System.Drawing.Point(7, 44);
+            this.txt_radButton.Name = "txt_radButton";
+            this.txt_radButton.Size = new System.Drawing.Size(71, 17);
+            this.txt_radButton.TabIndex = 1;
+            this.txt_radButton.TabStop = true;
+            this.txt_radButton.Text = "Alfabetisk";
+            this.txt_radButton.UseVisualStyleBackColor = true;
+            // 
+            // num_radButton
+            // 
+            this.num_radButton.AutoSize = true;
+            this.num_radButton.Location = new System.Drawing.Point(7, 20);
+            this.num_radButton.Name = "num_radButton";
+            this.num_radButton.Size = new System.Drawing.Size(69, 17);
+            this.num_radButton.TabIndex = 0;
+            this.num_radButton.TabStop = true;
+            this.num_radButton.Text = "Numerisk";
+            this.num_radButton.UseVisualStyleBackColor = true;
+            this.num_radButton.CheckedChanged += new System.EventHandler(this.num_radButton_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 221);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn_Backspace);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Backspace;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox wordListBox;
+        private System.Windows.Forms.CheckBox t9_checkBox;
+        private System.Windows.Forms.RadioButton txt_radButton;
+        private System.Windows.Forms.RadioButton num_radButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TextPhone/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextPhone
+{
+    public partial class Form1 : Form
+    {
+        char[][] btnChars = new char[8][];
+        DateTime start;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            num_radButton.Checked = true;
+            t9_checkBox.Enabled = false;
+            wordListBox.Enabled = false;
+
+
+            //Arrays med knapparnas tecken
+            btnChars[0] = new[] { 'a', 'b', 'c' };
+            btnChars[1] = new[] { 'd', 'e', 'f' };
+            btnChars[2] = new[] { 'g', 'h', 'i' };
+            btnChars[3] = new[] { 'j', 'k', 'l' };
+            btnChars[4] = new[] { 'm', 'n', 'o' };
+            btnChars[5] = new[] { 'p', 'q', 'r', 's' };
+            btnChars[6] = new[] { 't', 'u', 'v' };
+            btnChars[7] = new[] { 'w', 'x', 'y', 'z' };
+
+            btn0.Select();
+        }
+
+        private void num_radButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (num_radButton.Checked == true)
+            {
+                t9_checkBox.Enabled = false;
+                wordListBox.Enabled = false;
+            }
+            else
+            {
+                t9_checkBox.Enabled = true;
+                wordListBox.Enabled = true;
+            }
+        }
+
+        //Radera allt som står i textrutan
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+        }
+
+        //Tar endast bort det senaste tecknet i textrutan
+        private void btn_Backspace_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            }
+        }
+
+        //Händelsehanterare för knapp 0-9
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            int btnNum = Convert.ToInt32(b.Name.Substring(3));
+
+            start = DateTime.Now;
+            timer1.Start();
+
+            if (txt_radButton.Checked == true)
+            {
+                //Testa så funktionen fungerar
+                if (b.Name == "btn0" || b.Name == "btn1")
+                {
+
+                }
+                else
+                {
+                    textBox1.Text += btnChars[btnNum - 2][0].ToString();
+                }
+            }
+            else if (num_radButton.Checked == true)
+            {
+                textBox1.Text += b.Name.Substring(3);
+            }
+        }
+
+        public void tid()
+        {
+            timer1.Stop();
+
+            TimeSpan tid = DateTime.Now - start;
+
+            if (start.Millisecond > 0)
+            {
+                TimeSpan kontroll = TimeSpan.FromSeconds(0.7);
+                if (tid < kontroll)
+                {
+                    textBox1.Text = "test funkar";
+                }
+                else
+                {
+                    textBox1.Text = "tes funkar 2";
+                }
+            }
+        }
+    }
+}
